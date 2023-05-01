e1532332d5f0da1d78e206419262336465fced89
Akshay Gaikwad <132028709+AkshayEnginner@users.noreply.github.com>
2023-05-01 18:24:40 +0530

    Add files via upload

---
+++ Number_question_list.java
@@ -0,0 +1,34 @@
+Write a program to reverse an integer in Java.
+Write a program in Java to check whether an integer is Armstrong number or not.
+Write a program in Java to check given number is prime or not.
+Write a program in Java to print the Fibonacci series using iteration.
+Write a program in Java to print the Fibonacci series using recursion.
+Write a program in Java to check whether a number is Palindrome or not using iteration.
+Write a program in Java to check whether a number is Palindrome or not using recursion.
+Write a program in Java to find greatest among three integers.
+Write a program in Java to find sum of digits of a number using recursion?
+Write a program in Java to swap two numbers without using third variable?
+Write a program in Java to swap two numbers using third variable?
+Write a program in Java to find prime factors of a given integer.
+Write a program in Java to add two integer without using arithmetic operator?
+Write a program in Java to find given number is perfect or not?
+Java Program to Calculate the Average of Integer Array.
+Java Program to calculate the Average of number taken from user.
+Java Program to calculate factorial using iterative method.
+Java Program to calculate factorial using recursion.
+Java Program to check a given number is even or odd.
+Java program to print first n Prime Number with explanation.
+Java Program to print Prime Number in a given range.
+Java Program to find Smallest number among three.
+Java program to calculate the power using the POW method.
+Java Program to calculate the power without using POW function.
+Java Program to calculate the square of a given number.
+Java Program to calculate the cube of a given number.
+Java Program to calculate the square root of a given number.
+Java program to calculate LCM of given two numbers.
+Java Program to find GCD or HCF of two numbers.
+Java Program to find GCD of two numbers using recursion.
+Java Program to check the given year is a leap year or not.
+Java Program to convert Celsius to Fahrenheit.
+Java Program to convert Fahrenheit to Celsius.
+Java program to calculate Simple Interest with explanation.
