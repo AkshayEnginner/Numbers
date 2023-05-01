deddf72ef062aefb097e56b3353704e39f67ae73
Akshay Gaikwad <132028709+AkshayEnginner@users.noreply.github.com>
2023-05-01 18:30:29 +0530

    Add files via upload

---
+++ number8.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class number8
+{
+    public static void main(String args[])
+    {
+        int a=21,b=45,c=34;
+        
+        if(a>b && a>c)
+        {
+            System.out.println("highest number " +a);
+        }
+        else if(b>a && b>c)
+        {
+             System.out.println("highest number " +b);
+        }
+        else if(c>a && c>a)
+        {
+             System.out.println("highest number " +c);
+        }
+    }
+}
