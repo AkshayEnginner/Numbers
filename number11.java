a60d5103808ac9c1378600a36f6b1a082433a48e
Akshay Gaikwad <132028709+AkshayEnginner@users.noreply.github.com>
2023-05-02 18:54:39 +0530

    Add files via upload

---
+++ number11.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class number11
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a=5,b=7,temp;
+        System.out.println("before swapping a " +a+ "before swapping b " +b );
+        temp=a;
+        a=b;
+        b=temp;
+         System.out.println("after swapping a " +a+ "before swapping b " +b );
+        
+    }
+    
+}
