be441907a7e1d1f7c632ed3e9934218cc1f5be03
Akshay Gaikwad <132028709+AkshayEnginner@users.noreply.github.com>
2023-05-03 08:09:27 +0530

    Add files via upload

---
+++ number22.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class number22
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        {
+            int a=1;
+            int b=8;
+            int c=2;
+            if(a<b && a<c)
+            {
+                System.out.println("a is smallest number");
+            }
+            if(b<a && b<c)
+            {
+                System.out.println("b is smallest number");
+            }
+            if(c<b && c<a)
+            {
+                System.out.println("c is smallest number");
+            }
+        }
+    }
+}
